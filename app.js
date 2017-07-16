c624f13e1b88fecc8aa853c1f716ee3ad3af5847
BlackZIjian <735809461@qq.com>
2017-07-16 22:31:15 +0800

    解决跨域

--- app.js
+++ app.js
@@ -54,6 +54,19 @@ setInterval(function () {
   }
 },config.heartBeatCheckTime);
 
+app.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+    if (req.method == 'OPTIONS') {
+        res.send(200); 
+    }
+    else {
+        next();
+    }
+});
+
 app.use('/',function (req,res,next) {
     var deviceId = req.body.deviceId;
     if(deviceId){
